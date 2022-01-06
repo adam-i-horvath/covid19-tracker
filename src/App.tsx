3b0e56233bc44161ff6fe0a5b8ab39a29853a441
Ádám István Horváth <adam@AdamHorvathMBP.home>
2022-01-06 05:14:04 +0100

    graphql apollo ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+import { useQuery, gql } from '@apollo/client';
+
+interface Datatypes {
+  country: {
+    name: string;
+    cases: number;
+    deaths: number;
+    analysis: any;
+    todayCases: number;
+    recovered: string | number;
+  };
+}
+
+const COVID_DATA = gql`
+  query GetCovidData {
+    country(name: "Hungary") {
+      deaths
+      cases
+      todayCases
+      analysis {
+        cases
+        todayCases
+        recovered
+      }
+    }
+  }
+`;
+
+function GetCovidData() {
+  const { loading, error, data } = useQuery<Datatypes>(COVID_DATA);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading ...</p>
+      ) : (
+        <div>
+          <h1>COVID-19 in Hungary</h1>
+          <h3>JSON</h3>
+          {JSON.stringify(data)}
+
+          <h3>Data</h3>
+          <p>Cases: {data!.country.cases.toLocaleString('uk')}</p>
+          <p>Deaths: {data!.country.deaths.toLocaleString('uk')}</p>
+          <p>Today cases: {data!.country.todayCases.toLocaleString('uk')}</p>
+
+          <h3>ANALYSIS</h3>
+          <h4>Cases</h4>
+          <p>Status: {data!.country.analysis.cases.status}</p>
+          <p>Percentage: {data!.country.analysis.cases.percentage}</p>
+
+          <h4>Today cases</h4>
+          <p>Status: {data!.country.analysis.todayCases.status}</p>
+          <p>Percentage: {data!.country.analysis.todayCases.percentage}</p>
+
+          <h4>Recovered</h4>
+          <p>Status: {data!.country.analysis.recovered.status}</p>
+          <p>Percentage: {data!.country.analysis.recovered.percentage}</p>
+
+          <p>Used technologies: React, Typescript, Graphql, Apollo</p>
+          <span>Used API: https://corona-graphql.herokuapp.com/</span>
+        </div>
+      )}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GetCovidData />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://corona-graphql.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query {
+        country(name: "Hungary") {
+          deaths
+          cases
+          todayCases
+          analysis {
+            cases
+            todayCases
+            recovered
+          }
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
